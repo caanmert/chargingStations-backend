3032372c9b860bffa9954ebb9b6bc2eb0630ae06
canmert <canmert.dev@gmail.com>
2021-12-09 22:22:30 +0300

    Initial Commit

---
+++ src/main/java/com/canmert/chargingStations/ChargingStationsApplication.java
@@ -0,0 +1,13 @@
+package com.canmert.chargingStations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargingStationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargingStationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canmert/chargingStations/ChargingStationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canmert.chargingStations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargingStationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
