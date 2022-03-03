d915cd529c9086fe0e903e97721c33dbe8dd8a08
canmert <canmert.dev@gmail.com>
2022-03-03 12:29:27 +0300

    creating controller class for the user

---
+++ src/main/java/com/canmert/chargingStations/controller/UserRestController.java
@@ -0,0 +1,40 @@
+package com.canmert.chargingStations.controller;
+
+import com.canmert.chargingStations.model.User;
+import com.canmert.chargingStations.service.UserService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestController {
+
+    private final UserService userService;
+
+    public UserRestController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/users/{id}")
+    public User getOne(@PathVariable Long id) {
+        return userService.getById(id);
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user){
+        return userService.create(user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable Long id) {
+        return userService.delete(id);
+    }
+
+
+    
+}
