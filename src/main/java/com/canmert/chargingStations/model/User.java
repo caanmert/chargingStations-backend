2511cf62d349db814047b157101580d3c36562c2
canmert <canmert.dev@gmail.com>
2022-03-03 16:56:11 +0300

    Adding getters/setters, toString methods to the User Model

--- src/main/java/com/canmert/chargingStations/model/User.java
+++ src/main/java/com/canmert/chargingStations/model/User.java
@@ -1,5 +1,7 @@
 package com.canmert.chargingStations.model;
 
+import java.util.UUID;
+
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
@@ -23,4 +25,86 @@ public class User {
     public enum RoleType{
         BASIC,ADMIN
     }
+
+    public User() {}
+
+    public User(String username, String password, boolean enabled, boolean tokenExpired, RoleType role) {
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+        this.tokenExpired = tokenExpired;
+        this.role = role;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    public boolean getEnabled() {
+        return this.enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isTokenExpired() {
+        return this.tokenExpired;
+    }
+
+    public boolean getTokenExpired() {
+        return this.tokenExpired;
+    }
+
+    public void setTokenExpired(boolean tokenExpired) {
+        this.tokenExpired = tokenExpired;
+    }
+
+    public RoleType getRole() {
+        return this.role;
+    }
+
+    public void setRole(RoleType role) {
+        this.role = role;
+    }
+    
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", username='" + getUsername() + "'" +
+            ", password='" + getPassword() + "'" +
+            ", enabled='" + isEnabled() + "'" +
+            ", tokenExpired='" + isTokenExpired() + "'" +
+            ", role='" + getRole() + "'" +
+            "}";
+    }
+
+
+
 }
